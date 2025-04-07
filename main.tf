bac6bb2c05eeeb062a67b64c41e916c5302083bc
Iliyan Iliev <iliyan.hr.iliev@gmail.com>
2025-04-07 10:52:34 +0100

    format main.tf

--- main.tf
+++ main.tf
@@ -62,13 +62,13 @@ resource "azurerm_mssql_server" "sqlserver" {
 }
 
 resource "azurerm_mssql_database" "db" {
-  name           = "${var.sql_database_name}${random_integer.random.result}"
-  server_id      = azurerm_mssql_server.sqlserver.id
-  collation      = "SQL_Latin1_General_CP1_CI_AS"
-  license_type   = "LicenseIncluded"
-  max_size_gb    = 2
-  sku_name       = "S0"
-  zone_redundant = "false"
+  name               = "${var.sql_database_name}${random_integer.random.result}"
+  server_id          = azurerm_mssql_server.sqlserver.id
+  collation          = "SQL_Latin1_General_CP1_CI_AS"
+  license_type       = "LicenseIncluded"
+  max_size_gb        = 2
+  sku_name           = "S0"
+  zone_redundant     = "false"
   geo_backup_enabled = "false"
 }
 
